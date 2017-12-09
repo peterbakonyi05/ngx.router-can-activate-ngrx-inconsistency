9460e83dabfce2bc5dbca3f235eaa453776198c1
Peter Bakonyi <peter.bakonyi@betssongroup.com>
2017-12-09 15:13:20 +0100

    add subscription to `HomePage`

--- src/app/core/route/route.effect.ts
+++ src/app/core/route/route.effect.ts
@@ -1,5 +1,5 @@
 import { Observable } from 'rxjs/Observable';
-import { map, filter } from 'rxjs/operators';
+import { map, filter, tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { ResolveEnd, Router } from '@angular/router';
 import { Actions, Effect } from '@ngrx/effects';
@@ -14,6 +14,7 @@ export class RouteEffect {
   @Effect() login$: Observable<Action> = this.router.events
     .pipe(
     filter(e => e instanceof ResolveEnd),
+    tap(() => console.log('RESOLVE END EFFECT IS RUNNING')),
     map((e: ResolveEnd) => new route.ResolveEnd({ path: e.url }))
     );
 
--- src/app/pages/home.page.ts
+++ src/app/pages/home.page.ts
@@ -1,10 +1,28 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs/Subscription';
+
+import { CoreState, RouteSelector } from '../core/index';
 
 @Component({
   template: '<h1>HOME PAGE</h1>'
 })
-export class HomePage implements OnDestroy {
+export class HomePage implements OnInit, OnDestroy {
+  private s: Subscription;
+
+  constructor(
+    private store: Store<CoreState>,
+    private routeSelector: RouteSelector
+  ) { }
+
+  ngOnInit() {
+    this.s = this.store.select(this.routeSelector.getCurrent())
+      .subscribe(data => {
+        console.log('HomePage.routeSelector emit', data);
+      });
+  }
   ngOnDestroy() {
     console.log('HomePage.ngOnDestroy');
+    this.s.unsubscribe();
   }
 }
